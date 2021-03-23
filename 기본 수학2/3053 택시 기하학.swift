d330728119fcf94da6a72fed4ff76afd82a2d4c6
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-03-24 01:34:25 +0900

    3/24 기본수학2 추가

---
+++ 기본 수학2/3053 택시 기하학.swift
@@ -0,0 +1,14 @@
+//
+//  3053 택시 기하학.swift
+//  swift_bj
+//
+//  Created by Kang Minsang on 2021/03/24.
+//
+
+import Foundation
+func main10() {
+    let R = Double(readLine()!)!
+    let pi = 3.141592653589793
+    print(String(format: "%.6f", pi*R*R))
+    print(String(format: "%.6f", 2*R*R))
+}
