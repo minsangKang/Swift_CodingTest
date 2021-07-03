bc5be40544ca30eb6f8cb9fc55f09a92bfaee174
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-07-03 13:09:24 +0900

    7/3 이코테-DBFS 추가(시간초과 해결)

--- 나동빈 - 이코테/DFS BFS/특정 거리의 도시 찾기.swift
+++ 나동빈 - 이코테/DFS BFS/특정 거리의 도시 찾기.swift
@@ -53,32 +53,72 @@ func main() {
     let X: Int = datas[3] //출발위치
     var graph: [[Int]] = Array(repeating: [], count: N+1)
     var passed: [Int] = Array(repeating: -1, count: N+1)
+
+    for _ in 0..<M {
+        let input = readLine()!.split(separator: " ").map() { Int(String($0))! }
+        graph[input[0]].append(input[1])
+    }
+
+    var results: [Int] = []
+    var queue = Queue<Int>()
+    queue.enqueue(X)
+    passed[X] = 0
+    while(!queue.isEmpty) {
+        let start = queue.dequeue()!
+        if(passed[start] >= K) { continue } //문제
+
+        for go in graph[start] {
+            if(passed[go] == -1) {
+                passed[go] = passed[start]+1
+                queue.enqueue(go)
+                if(passed[start]+1 == K) { results.append(go) } //문제
+            }
+        }
+    }
+
+    results.sort()
+    if(results.isEmpty) { print(-1) }
+    else {
+        for i in results { print(i) }
+    }
+
+}
+
+func main() {
+    let datas = readLine()!.split(separator: " ").map() { Int(String($0))! }
+    let N: Int = datas[0] //도시수
+    let M: Int = datas[1] //간선수
+    let K: Int = datas[2] //구하는 거리값
+    let X: Int = datas[3] //출발위치
+    var graph: [[Int]] = Array(repeating: [], count: N+1)
+    var passed: [Int] = Array(repeating: -1, count: N+1)
     
     for _ in 0..<M {
         let input = readLine()!.split(separator: " ").map() { Int(String($0))! }
         graph[input[0]].append(input[1])
     }
     
-    var queue = Queue<Int>()
-    queue.enqueue(X)
+    var results: [Int] = []
+    var queue: [Int] = []
+    queue.append(X)
     passed[X] = 0
     while(!queue.isEmpty) {
-        let start = queue.dequeue()!
+        let start = queue.removeFirst()
+        if(passed[start] >= K) { continue } //문제
+        
         for go in graph[start] {
             if(passed[go] == -1) {
                 passed[go] = passed[start]+1
-                queue.enqueue(go)
+                queue.append(go)
+                if(passed[start]+1 == K) { results.append(go) } //문제
             }
         }
     }
     
-    var check: Bool = true
-    for i in 1...N where passed[i] == K {
-        check = false
-        print(i)
-    }
-    if(check) {
-        print(-1)
+    results.sort()
+    if(results.isEmpty) { print(-1) }
+    else {
+        for i in results { print(i) }
     }
     
 }
