59a8d1af16442a43229e2758554a48432f591965
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-07-03 12:47:06 +0900

    7/3 이코테-DBFS 추가(시간초과)

---
+++ 나동빈 - 이코테/DFS BFS/특정 거리의 도시 찾기.swift
@@ -0,0 +1,84 @@
+//
+//  특정 거리의 도시 찾기.swift
+//  swift_bj
+//
+//  Created by Kang Minsang on 2021/07/03.
+//
+
+import Foundation
+
+public struct Queue<T> {
+    var array = [T?]()
+    fileprivate var head = 0
+
+    public var isEmpty: Bool {
+        return count == 0
+    }
+
+    public var count: Int {
+        return array.count - head
+    }
+
+    public mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+
+    public mutating func dequeue() -> T? {
+        guard head < array.count, let element = array[head] else { return nil }
+
+        array[head] = nil
+        head += 1
+
+        let percentage = Double(head)/Double(array.count)
+        if (array.count > 50 || percentage > 0.25) {
+            array.removeFirst(head)
+            head = 0
+        }
+        return element
+    }
+    public var front: T? {
+        if isEmpty {
+            return nil
+        } else {
+            return array[head]
+        }
+    }
+}
+
+func main() {
+    let datas = readLine()!.split(separator: " ").map() { Int(String($0))! }
+    let N: Int = datas[0] //도시수
+    let M: Int = datas[1] //간선수
+    let K: Int = datas[2] //구하는 거리값
+    let X: Int = datas[3] //출발위치
+    var graph: [[Int]] = Array(repeating: [], count: N+1)
+    var passed: [Int] = Array(repeating: -1, count: N+1)
+    
+    for _ in 0..<M {
+        let input = readLine()!.split(separator: " ").map() { Int(String($0))! }
+        graph[input[0]].append(input[1])
+    }
+    
+    var queue = Queue<Int>()
+    queue.enqueue(X)
+    passed[X] = 0
+    while(!queue.isEmpty) {
+        let start = queue.dequeue()!
+        for go in graph[start] {
+            if(passed[go] == -1) {
+                passed[go] = passed[start]+1
+                queue.enqueue(go)
+            }
+        }
+    }
+    
+    var check: Bool = true
+    for i in 1...N where passed[i] == K {
+        check = false
+        print(i)
+    }
+    if(check) {
+        print(-1)
+    }
+    
+}
